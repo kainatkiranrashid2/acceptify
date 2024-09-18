e4c440d1cd385086b1156b31a12ef6df15884ca8
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-09-18 18:32:15 +0500

    acceptify deploy

--- src/components/reuseable_components/AnimatedHeading.jsx
+++ src/components/reuseable_components/AnimatedHeading.jsx
@@ -21,32 +21,34 @@ const AnimatedHeading = ({
         }}>
         {firstText}
       </motion.div>
-      <motion.div
-        className={`text-${headingSize} text-black dark:text-white mr-2`}
-        style={{
-          letterSpacing: "-2px",
-        }}>
-        {secondText}
-      </motion.div>
-
-      <motion.div
-        key={currentText}
-        className={`text-${headingSize} rounded-2xl h-[${heightForBox}]  bg-primary font-PP_Mori text-nowrap overflow-hidden text-white border-4 border-[#65D008] inline-flex items-center justify-center`}
-        style={{ lineHeight: "85px" }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}>
-        <motion.p
-          className={`text-${headingSize} inline-block h-full   my-0 px-6 font-PP_Mori`}
-          initial={{ y: "100%" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 2 }}
+      <div className="flex ">
+        <motion.div
+          className={`text-${headingSize} text-black dark:text-white mr-2`}
           style={{
             letterSpacing: "-2px",
           }}>
-          {currentText}
-        </motion.p>
-      </motion.div>
+          {secondText}
+        </motion.div>
+
+        <motion.div
+          key={currentText}
+          className={`text-${headingSize} rounded-2xl h-[${heightForBox}]  bg-primary font-PP_Mori text-nowrap overflow-hidden text-white border-4 border-[#65D008] inline-flex items-center justify-center`}
+          style={{ lineHeight: "85px" }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}>
+          <motion.p
+            className={`text-${headingSize} inline-block h-full   my-0 px-6 font-PP_Mori`}
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 2 }}
+            style={{
+              letterSpacing: "-2px",
+            }}>
+            {currentText}
+          </motion.p>
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
