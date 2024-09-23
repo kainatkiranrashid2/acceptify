b7ceaa2571283bc525b68afae4359e5a304c6203
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-09-23 21:15:42 +0500

    add responsiveness for the slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,49 +6,6 @@ import ScrollAnimationComponent from "../../components/ScrollAnimationComponent/
 import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 
 const Home = () => {
-  const items = [
-    {
-      id: 0,
-      icon: {
-        src: "/assets/whylsa/lock_svg.svg",
-        alt: "online_svg",
-      },
-      title: "Simplify Your PCI",
-      details:
-        "Simplify your PCI-DSS Compliance with our P2PE solution. P2PE means you get to skip   75% of the PCI-DSS certification steps vastly reducing audits making them less complex, less costly, and much quicker.",
-    },
-    {
-      id: 1,
-      icon: {
-        src: "/assets/whylsa/no_wifi_svg.svg",
-        alt: "no_wifi_svg",
-      },
-      title: "Online / Offline",
-      details:
-        "No connection, no problem. acceptify intelligently ensures that all your transactions are swift, secure, and trackable, whether your devices are online or offline.   ",
-    },
-    {
-      id: 2,
-      icon: {
-        src: "/assets/whylsa/online_svg.svg",
-        alt: "online_svg",
-      },
-      title: "iOS, Android or Windows",
-      details:
-        "Whether you have iOS, Android, or Windows devices (Mobile or Tablet), acceptify simply integrates and supports all platforms.",
-    },
-    {
-      id: 3,
-      icon: {
-        src: "/assets/whylsa/wristband_svg.svg",
-        alt: "Accept Wristbands",
-      },
-      title: "Accept Wristbands",
-      details:
-        "Go Cardless. Issue RFID wristbands to your customers, and automatically assign a payment card improving revenue, checkout speed, customer loyalty and customer experience.",
-    },
-  ];
-  console.log(items);
   return (
     <>
       <div>
